e0b7f55c644e8eb5e6ffb7b2ea87d9d1ee1580ca
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-08-09 17:52:51 +0000

    d.k

--- src/Header.tsx
+++ src/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
     };
     return (
         <div>
-            <div className="bg-yellow-600 text-center py-2 lg:px-4 p-2 bg-yellow-500 items-center text-white flex">
+            <div className="bg-yellow-600 text-center py-2 lg:px-4 p-2 bg-yellow-600 items-center text-white flex">
                     <span className="mr-2 text-center flex-auto">Get exclusive offers for a limited time</span>
             </div>
             <div className="header border-b border-gray-200 bg-white mx-auto py-5 px-5 sm:py-3 sm:px-3 lg:px-10">
